22f4aaa85831ff3b18f8daa700c5e285f00661eb
WilliamSmith999 <williamsmithtannis013@gmail.com>
2018-08-24 23:20:15 -0500

    Engaged old work

--- Expenses/ExpensesViewController.swift
+++ Expenses/ExpensesViewController.swift
@@ -6,4 +6,47 @@
 //  Copyright © 2018 William Smith. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+class ExpensesViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    let Expenses = ["Iamlazy"]
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        tableView.dataSource = self
+        tableView.delegate = self
+    }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return Expenses.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ExpensesCell", for: indexPath)
+        let foodItem = Expenses[indexPath.row]
+        cell.textLabel?.text = foodItem
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let food = Expenses[indexPath.row]
+        print(food)
+        
+        
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+}
